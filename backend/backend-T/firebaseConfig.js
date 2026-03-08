2d3913e88d549444f4baafbcc64e7313321c788d
Thanushan <thanushan.20240138@iit.ac.lk>
2026-03-08 20:50:12 +0530

    backend files updated

---
+++ backend/backend-T/firebaseConfig.js
@@ -0,0 +1,19 @@
+// frontend/firebaseConfig.js
+// This file connects the mobile app to Firebase database
+// Separate from backend — same Firebase project, but used in the app
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDJOtHU6FY4WosW_exH1q-CN6JyU0OmXZI",
+  authDomain: "navi-kid-school-van-tracker.firebaseapp.com",
+  projectId: "navi-kid-school-van-tracker",
+  storageBucket: "navi-kid-school-van-tracker.firebasestorage.app",
+  messagingSenderId: "997311667098",
+  appId: "1:997311667098:web:8074b1381220c37cbe7823",
+  measurementId: "G-8MP3BEFT1C"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
